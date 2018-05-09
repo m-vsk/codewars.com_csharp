44d6a9b177094c7280b11f096b83a256d762fb8c
vsk <m-vsk@yandex.ru>
2018-05-09 23:20:47 +0300

    Update code

---
+++ GetMiddleCharacter/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMiddleCharacter
+{
+    class Kata
+    {
+
+        public static string getMiddle(string s)
+        {
+            int length = s.Length;
+
+            if (length == 1) return s;
+
+            if (length % 2 == 0)
+            {
+                return s.Substring(length / 2 - 1, 2);
+            }
+            else
+            {
+                return s.Substring(length / 2, 1);
+            }
+        }
+
+    }
+}
--- GetMiddleCharacter/Program.cs
+++ GetMiddleCharacter/Program.cs
@@ -10,7 +10,14 @@ namespace GetMiddleCharacter
     {
         static void Main(string[] args)
         {
-            //
+
+            Console.WriteLine(Kata.getMiddle("test"));
+            Console.WriteLine(Kata.getMiddle("testing"));
+            Console.WriteLine(Kata.getMiddle("middle"));
+            Console.WriteLine(Kata.getMiddle("A"));
+
+
+            Console.ReadKey();
         }
     }
 }
